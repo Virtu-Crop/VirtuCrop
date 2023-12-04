afd768b94a0816edc8a96a023b0256f0ef767666
SUSHANT0002 <sushant.mahadwad@gmail.com>
2023-12-04 20:08:10 +0530

    start

---
+++ website/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <div>
+      <img src="./src/assets/logo2.png" alt="" />
+    </div>
+  )
+};
+
+
+export default Home
---
+++ website/src/Pages/Info/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Info.scss';
+const Info = () => {
+  return (
+    <div>
+      info
+    </div>
+  )
+}
+
+export default Info
