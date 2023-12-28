2610295def1f2311c84754e32952c41c08b06fe9
SUSHANT0002 <sushant.mahadwad@gmail.com>
2023-12-28 23:35:35 +0530

    the site is ready to host with backend connection

---
+++ Backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+//create database connection
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'virtucrop'
+});
+
+
+app.post('/contact',(req,res)=>{
+    const { name, email, phone, message } = req.body;
+    const insertQuery = 'INSERT INTO contact (`name`, `email`, `phone`, `message`) VALUES (?, ?, ?, ?)';
+    conn.query(insertQuery, [name, email,phone, message], (err, result) => {
+        if (err) {
+            console.error('Error inserting data into database: ' + err.message);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        console.log('Data inserted into database');
+        res.status(200).send('Form submitted successfully');
+    });
+});
+
+//listen to port
+app.listen(5000, () => {
+        console.log(`Server running successfully on 5000`);
+    });
--- MainWebsite/src/Pages/Contact/Contact.jsx
+++ MainWebsite/src/Pages/Contact/Contact.jsx
@@ -1,65 +1,64 @@
 import React, { useState } from 'react';
 import './Contact.scss';
+import axios from 'axios';
 
 const Contact = () => {
-  const [isVisible,setIsVisible]=useState(false);
-  const [buttonName,setButtonName]=useState('Contact');
-  const [data,setData]=useState({name:'',email:'',phone:'',message:''});
-  function handleChange(e){
-      const name = e.target.name;
-      const value=e.target.value;
-      setData({...data, [name]:value})
-  }
-  function handleSubmit(e){
-    e.preventDefault()
-    console.log(data)
-    alert('done')
-  }
 
-//   function showForm(){
-//     const btn = document.getElementById('button');
-//     const show = document.getElementById('form');
-//     if(show.style.display==='none'){
-//       show.style.display='flex';
-//       btn.innerHTML='Close';
-      
-//     }
-//     else{
-//       show.style.display='none';
-//       btn.innerHTML='Contact';
-//     }
-// }
-function showForm(){
-  if(isVisible===false){
-  setIsVisible(!isVisible);
-  setButtonName('Close Form')
-}else{
-  setIsVisible(!isVisible);
-  setButtonName('Contact')
-}
-}
+  //sql connection sending data to port 5000
+  const [data, setData] = useState({ name: '', email: '', phone: '', message: '' });
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/contact', data);
+      console.log('Form Submitted Successfully');
+      setData({ name: '', email: '', phone: '', message: '' });
+    } catch (error) {
+      console.error('Error submitting form: ' + error.message);
+      alert("Form Not Submitted"+ error.message);
+    }
+  };
+
+
+
+  // for displaying form
+  const [isVisible, setIsVisible] = useState(false);
+  const [buttonName, setButtonName] = useState('Contact');
+  function showForm() {
+    if (isVisible === false) {
+      setIsVisible(!isVisible);
+      setButtonName('Close Form')
+    } else {
+      setIsVisible(!isVisible);
+      setButtonName('Contact')
+    }
+  }
   return (
     <>
       <div className='contact md:flex-col  sm:flex-col'>
-      <p>Ready to elevate your business with Virtucrop? <br />Contact us today to explore how our expertise can drive your success.</p>
+        <p>Ready to elevate your business with Virtucrop? <br />Contact us today to explore how our expertise can drive your success.</p>
         <button onClick={showForm}>{buttonName}</button>
-        {isVisible && <form onSubmit={handleSubmit} method='post' id='form'  className='flex flex-col'>
+        {isVisible && <form onSubmit={handleSubmit} method='post' id='form' className='flex flex-col'>
           <h1>CONTACT</h1>
           {/* <label for="name">Name:</label> */}
           <input type="text" id="name" onChange={handleChange} value={data.name} placeholder='Name' name="name" required />
-            {/* <label for="email">Email:</label> */}
-            <input type="email" id="email" onChange={handleChange} value={data.email} placeholder='E-mail' name="email" required />
-            <input type="number" id="number" onChange={handleChange} value={data.phone} placeholder='+91' name="phone" required />
-              {/* <label for="message">Message:</label> */}
-              <textarea id="message" name="message" onChange={handleChange} value={data.message} placeholder='Message' rows="4" required ></textarea>
-              <button type="submit" >Submit</button>
-
-              {/* <p>{data.name},{data.email},{data.phone},{data.message}</p> */}
-            </form>
-            }
-          </div>
-        </>
-        )
+          {/* <label for="email">Email:</label> */}
+          <input type="email" id="email" onChange={handleChange} value={data.email} placeholder='E-mail' name="email" required />
+          {/* <label for="phone">Phone:</label> */}
+          <input type="tel" id="number" onChange={handleChange} value={data.phone} placeholder='+91' name="phone" required />
+          {/* <label for="message">Message:</label> */}
+          <textarea id="message" name="message" onChange={handleChange} value={data.message} placeholder='Message' rows="4" required ></textarea>
+          <button type="submit" >Submit</button>
+          {/* <p>{data.name},{data.email},{data.phone},{data.message}</p> */}
+        </form>
+        }
+      </div>
+    </>
+  )
 }
 
-        export default Contact
+export default Contact
